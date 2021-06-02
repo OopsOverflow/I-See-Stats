bfd6550a204c484196042f4f93d8a2876c7ac4f0
Houssem Mahmoud <74513172+OopsOverflow@users.noreply.github.com>
2021-06-02 09:41:43 +0200

    Added Skybox to Earth View, Change absolute path, still didn't get around that

---
+++ src/sample/EarthTest.java
@@ -0,0 +1,123 @@
+package sample;
+import com.interactivemesh.jfx.importer.ImportException;
+import com.interactivemesh.jfx.importer.obj.ObjModelImporter;
+import javafx.application.Application;
+import javafx.geometry.Point3D;
+import javafx.scene.*;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.MeshView;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
+import javafx.stage.Stage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+
+public class EarthTest extends Application {
+
+    private static final float TEXTURE_LAT_OFFSET = -0.2f;
+    private static final float TEXTURE_LON_OFFSET = 2.8f;
+
+    @Override
+    public void start(Stage primaryStage) throws FileNotFoundException {
+
+        //Create a Pane et graph scene root for the 3D content
+        Group root3D = new Group();
+        Pane pane3D = new Pane(root3D);
+
+        // Load geometry
+        ObjModelImporter objModelImporter = new ObjModelImporter();
+        try {
+            URL modelURL = this.getClass().getResource("Earth/earth.obj");
+
+            objModelImporter.read(modelURL);
+        } catch (ImportException e) {
+            System.out.println(e.getMessage());
+        }
+        MeshView[] meshViews = objModelImporter.getImport();
+        Group earth = new Group(meshViews);
+        // Draw a line
+
+        // Draw an helix
+
+        // Draw city on the earth
+
+
+        // Add a camera group
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        new CameraManager(camera, pane3D, root3D);
+
+        // Add point light
+        PointLight light = new PointLight(Color.WHITE);
+        light.setTranslateX(-180);
+        light.setTranslateY(-90);
+        light.setTranslateZ(-120);
+        light.getScope().addAll(root3D);
+        root3D.getChildren().add(light);
+//        String cwd = System.getProperty("user.dir");
+//        System.out.println(cwd + "\\files\\skybox\\nx.png");
+        FileInputStream stream = new FileInputStream("C:\\Users\\houss\\IdeaProjects\\tuto\\src\\sample\\skybox\\nx(1).png");
+        Image image = new Image(stream);
+        Skybox skybox = new Skybox(image, image, image, image, image, image, 512, camera);
+
+        // Add ambient light
+        AmbientLight ambientLight = new AmbientLight(Color.WHITE);
+        ambientLight.getScope().addAll(root3D);
+        root3D.getChildren().add(ambientLight);
+        root3D.getChildren().addAll(earth);
+        pane3D.getChildren().addAll(skybox);
+        // Create scene
+        Scene scene = new Scene(pane3D, 600, 600, true);
+        scene.setCamera(camera);
+        scene.setFill(Color.gray(0.2));
+
+        //Add the scene to the stage and show it
+        primaryStage.setTitle("Earth Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    // From Rahel Lüthy : https://netzwerg.ch/blog/2015/03/22/javafx-3d-line/
+    public Cylinder createLine(Point3D origin, Point3D target) {
+        Point3D yAxis = new Point3D(0, 1, 0);
+        Point3D diff = target.subtract(origin);
+        double height = diff.magnitude();
+
+        Point3D mid = target.midpoint(origin);
+        Translate moveToMidpoint = new Translate(mid.getX(), mid.getY(), mid.getZ());
+
+        Point3D axisOfRotation = diff.crossProduct(yAxis);
+        double angle = Math.acos(diff.normalize().dotProduct(yAxis));
+        Rotate rotateAroundCenter = new Rotate(-Math.toDegrees(angle), axisOfRotation);
+
+        Cylinder line = new Cylinder(0.01f, height);
+
+        line.getTransforms().addAll(moveToMidpoint, rotateAroundCenter);
+
+        return line;
+    }
+
+    public static Point3D geoCoordTo3dCoord(float lat, float lon) {
+        float lat_cor = lat + TEXTURE_LAT_OFFSET;
+        float lon_cor = lon + TEXTURE_LON_OFFSET;
+        return new Point3D(
+                -java.lang.Math.sin(java.lang.Math.toRadians(lon_cor))
+                        * java.lang.Math.cos(java.lang.Math.toRadians(lat_cor)),
+                -java.lang.Math.sin(java.lang.Math.toRadians(lat_cor)),
+                java.lang.Math.cos(java.lang.Math.toRadians(lon_cor))
+                        * java.lang.Math.cos(java.lang.Math.toRadians(lat_cor)));
+    }
+
+}
