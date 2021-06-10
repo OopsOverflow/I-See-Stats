33cf9c7da6020887f9a096540ea3a30587cc0069
Pierre Surer <pierre.surer@u-psud.fr>
2021-06-10 15:02:36 +0200

    Finished SpeciesData

--- src/model/Region.java
+++ src/model/Region.java
@@ -5,4 +5,12 @@ import model.GeoHash;
 public class Region {
     private int count;
     private GeoHash area;
+
+    /**
+     *
+     * @return number of species in the geohash
+     */
+    public int getCount() {
+        return count;
+    }
 }
--- src/model/SpeciesData.java
+++ src/model/SpeciesData.java
@@ -5,26 +5,58 @@ import java.util.Date;
 
 public class SpeciesData {
     private int precision;
-    private Species species;
+    private ArrayList<Species> species;
     private ArrayList<Region> regions;
     private Date date;
 
+    /**
+     *
+     * @return minimum count of species in a region
+     */
     public int getMinCount(){
-
+        int min = Integer.MAX_VALUE;
+        for(Region region : regions){
+            if(region.getCount()<min)min=region.getCount();
+        }
+        return min;
     }
 
+    /**
+     *
+     * @return maximum count of species in a region
+     */
     public int getMaxCount(){
-
+        int max = Integer.MIN_VALUE;
+        for(Region region : regions){
+            if(region.getCount()>max)max=region.getCount();
+        }
+        return max;
     }
 
-    public int getMaxCount(){
-
+    /**
+     *
+     * @return total count of species in region
+     */
+    public int getTotalCount(){
+        int total = 0;
+        for(Region region : regions){
+            total+=region.getCount();
+        }
+        return total;
     }
 
-    public Region getRegions(){
-
+    /**
+     *
+     * @return regions where the species are
+     */
+    public ArrayList<Region> getRegions() {
+        return regions;
     }
 
+    /**
+     *
+     * @return date of the measure
+     */
     public Date getDate() {
         return date;
     }
