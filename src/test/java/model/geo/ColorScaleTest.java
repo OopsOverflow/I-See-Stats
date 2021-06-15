ff346f3a391b90a8e2bd2e9988c6d615a9912a77
Houssem Mahmoud <74513172+OopsOverflow@users.noreply.github.com>
2021-06-15 19:46:03 +0200

    Test (What's left of them)
    All thanks to @PignoulMarcel

--- src/test/java/model/geo/ColorScaleTest.java
+++ src/test/java/model/geo/ColorScaleTest.java
@@ -1,20 +1,28 @@
 package model.geo;
 
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColorScaleTest {
 
+    ColorScale colorScale = new ColorScale();
+
     @Test
     void getColor() {
+        ArrayList<Color> test = new ArrayList<>();
+        test.add(Color.LEMONCHIFFON);
+        colorScale.setColors(test);
+        Assertions.assertEquals(Color.LEMONCHIFFON, colorScale.getColor(0));
     }
 
     @Test
     void setRange() {
+        Assertions.assertThrows(Exception.class, () -> {colorScale.setRange(500,200);});
     }
 
-    @Test
-    void setColors() {
-    }
 }
--- src/test/java/model/species/SpeciesDataTest.java
+++ src/test/java/model/species/SpeciesDataTest.java
@@ -1,11 +1,20 @@
 package model.species;
 
+import model.geo.GeoHash;
+import model.geo.Region;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpeciesDataTest {
-
+    ArrayList<Region> testArray = new ArrayList<>();
+    testArray.add()
+    SpeciesData speciesData = new SpeciesData(696969,
+            new Species(), new Date(2020,9, 21),
+            );
     @Test
     void getMinCount() {
     }
