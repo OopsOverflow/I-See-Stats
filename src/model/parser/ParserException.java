48efc32bb9a63a0cbe54dc26abb796f0b8bec377
mathis <mathis.brossier@universite-paris-saclay.fr>
2021-06-15 12:37:37 +0200

    removed old files

--- src/model/parser/ParserException.java
+++
@@ -1,9 +0,0 @@
-package model.parser;
-
-public class ParserException extends Exception {
-	private static final long serialVersionUID = 2804740549723047963L;
-	
-	public ParserException(String msg) {
-		super(msg);
-	}
-}
