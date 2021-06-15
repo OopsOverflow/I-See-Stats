2f56889a6809dcd16148e1e09c5adc1464137da3
Houssem Mahmoud <74513172+OopsOverflow@users.noreply.github.com>
2021-06-15 21:56:50 +0200

    What's left of my test after the great Marcel Massacre.

--- src/test/java/model/geo/GeoHashTest.java
+++ src/test/java/model/geo/GeoHashTest.java
@@ -1,29 +1,37 @@
 package model.geo;
 
+import javafx.geometry.Point2D;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GeoHashTest {
+    GeoHash geoHash = GeoHash.fromString("12345");
 
     @Test
     void fromString() {
+        Assertions.assertThrows(RuntimeException.class, () -> {GeoHash.fromString("12a3");});
     }
 
     @Test
     void getPrecision() {
+
     }
 
     @Test
     void getString() {
+        Assertions.assertEquals(geoHash.getString(), "12345");
     }
 
     @Test
     void testToString() {
+        Assertions.assertEquals(geoHash.toString(), "12345");
     }
 
     @Test
     void getLatLon() {
+        Assertions.assertEquals(geoHash.getLatLon(), new Point2D(-88.2421875, -122.2119140625));
     }
 
     @Test
